cda1c03974212b71e8d5ab1c61da89278297f496
Marco Gasparetto <m.gasparetto@app3.it>
2011-09-17 12:06:32 +0200

    Initial import.

---
+++ HackAzure/HackAzureAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HackAzureAppDelegate.h
+//  HackAzure
+//
+//  Created by Marco Gasparetto on 17/09/11.
+//  Copyright 2011 App3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HackAzureAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ HackAzure/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  HackAzure
+//
+//  Created by Marco Gasparetto on 17/09/11.
+//  Copyright 2011 App3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
